83c511fd5507ffeba1af65e7c809a0b7ec053ea3
Brayan28P <Brayan28P@gmail.com>
2020-09-07 23:50:39 -0500

    Creación de los métodos para logearse y redireccionar en el BEAN Y Manager

--- condominioEJB/ejbModule/condominio/modulos/login/model/ManagerLogin.java
+++ condominioEJB/ejbModule/condominio/modulos/login/model/ManagerLogin.java
@@ -1,9 +1,14 @@
 package condominio.modulos.login.model;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import condominio.core.model.entities.Usuario;
 
 /**
  * Session Bean implementation class ManagerLogin
@@ -17,4 +22,31 @@ public class ManagerLogin {
         // TODO Auto-generated constructor stub
     }
 
+    
+    public LoginDto comprobarCredenciales(String correo,String contrasenia) throws Exception {
+    	LoginDto login=new LoginDto();
+    	List<Usuario>usuarios=existeUsuario( correo, contrasenia);
+    Usuario usuario;
+    	if (usuarios.isEmpty()) {
+    		throw new Exception("Credenciales incorrectas intente otra vez");
+    	}else {
+    		usuario=usuarios.get(0);
+    	login.setIdUsuario(usuario.getIdusuario());
+    	login.setNombres(usuario.getApellidos()+" "+usuario.getNombres());
+    	login.setNombreRol(usuario.getRol().getNombre());
+    	login.setActivo(true);
+    	}
+    	return login;
+    	
+    }
+	@SuppressWarnings("unchecked")
+	public List<Usuario> existeUsuario(String correo,String contrasenia){
+		String JPQL = "SELECT u FROM Usuario u WHERE u.email=?1 and u.contrasenia=?2";
+		Query query = em.createQuery(JPQL, Usuario.class);
+		query.setParameter(1, correo);
+		query.setParameter(2, contrasenia);
+		List<Usuario> lista;
+		lista = query.getResultList();
+		return lista;	
+	}
 }
--- condominioWeb/src/condominio/modulos/login/view/controller/BeanLogin.java
+++ condominioWeb/src/condominio/modulos/login/view/controller/BeanLogin.java
@@ -1,9 +1,16 @@
 package condominio.modulos.login.view.controller;
 
 
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+
+import condominio.modulos.login.model.LoginDto;
+import condominio.modulos.login.model.ManagerLogin;
+import condominio.modulos.util.view.controller.JSFUtil;
+import condominio.modulos.util.view.controller.Seguridad;
+
 import java.io.Serializable;
 
 @Named
@@ -11,13 +18,81 @@ import java.io.Serializable;
 public class BeanLogin implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+private String correo;
+private String contrasenia;
+private LoginDto login;
 
+@EJB ManagerLogin managerLogin;
 	public BeanLogin() {
 		
 	}
 	public String  actionCerrarSesion() {
 		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();		
 		return "/login?faces-redirect=true";
+	}
+	
+	public String actionLogin() {
+		try {
+			contrasenia=Seguridad.encriptar(contrasenia);
+			System.out.println("Contrasenia que ingresa "+contrasenia);
+			login = managerLogin.comprobarCredenciales(correo, contrasenia);
+			correo="";
+			contrasenia="";
+			if (!login.isActivo()) {
+				JSFUtil.crearMensajeError("Cuenta inactiva contáctese con el administrador");
+				return"";
+			}
+			System.out.println("Rol; "+login.getNombreRol());
+			if (login.getNombreRol().equals("Tesorero")) {
+			/*
+				if (login.isIsnuevo()) {
+					return "administrador/seguridadAdministrador?faces-redirect=true";
+				}
+				*/
+				 
+				return "tesorero/menu?faces-redirect=true";
+			} else {
+				if (login.getNombreRol().equals("Condominio")) {
+					/*
+					if (login.isIsnuevo()==true) {
+						return "investigador/seguridadInvestigador?faces-redirect=true";
+					
+					}*/ 
+					return "condominio/menu?faces-redirect=true";
+				} else {
+					System.out.println("Rol; "+login.getNombreRol());
+					/*
+					if (login.isIsnuevo()==true) {
+						System.out.println("Ronuevol; "+login.getRol());
+						return "usuario/seguridadUsuario?faces-redirect=true";
+					}
+					*/ 
+					System.out.println("Rno; "+login.getNombreRol());
+					return "administrador/menu?faces-redirect=true";
+				}
+			}
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			return "";
+		}
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	public LoginDto getLogin() {
+		return login;
+	}
+	public void setLogin(LoginDto login) {
+		this.login = login;
 	}
 	
 }
