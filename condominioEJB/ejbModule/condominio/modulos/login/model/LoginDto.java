d6a9b2b6809e30b0a22971b6308fb45fbecedaa5
Brayan28P <Brayan28P@gmail.com>
2020-09-07 23:49:56 -0500

    Creación del DTO login

---
+++ condominioEJB/ejbModule/condominio/modulos/login/model/LoginDto.java
@@ -0,0 +1,34 @@
+package condominio.modulos.login.model;
+
+public class LoginDto {
+
+	private long idUsuario;
+	private String nombres;
+	private String nombreRol;
+	private boolean activo;
+	public long getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getNombreRol() {
+		return nombreRol;
+	}
+	public void setNombreRol(String nombreRol) {
+		this.nombreRol = nombreRol;
+	}
+	public boolean isActivo() {
+		return activo;
+	}
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
+}
