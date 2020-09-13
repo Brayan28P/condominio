402b9e334bfd969e545d5f29fa1b9d6539e8cb99
Guido Calderón <guical_96@hotmail.com>
2020-09-13 18:20:46 -0500

    Tipo gasto

---
+++ condominioWeb/src/condominio/modulos/tesorero/view/controller/BeanTipoGasto.java
@@ -0,0 +1,101 @@
+package condominio.modulos.tesorero.view.controller;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import condominio.core.model.entities.TipoGasto;
+import condominio.modulos.tesorero.model.ManagerTesorero;
+import condominio.modulos.util.view.controller.JSFUtil;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class BeanTipoGasto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private List<TipoGasto> listaTipoGastos;
+	private TipoGasto tipoGasto = new TipoGasto();
+	private TipoGasto editarTipoGasto = new TipoGasto();
+
+	@EJB
+	ManagerTesorero managerTesorero;
+
+	@PostConstruct
+	public void init() {
+		try {
+			listaTipoGastos = managerTesorero.findAllTipoGastos();
+		} catch (Exception e) {
+			e.getStackTrace();
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public void actionListenerCargarTipoGasto(TipoGasto g) {
+		editarTipoGasto = g;
+	}
+
+	public void actionListenerIngresarTipoGasto() {
+		try {
+			System.out.print(tipoGasto.getNombre());
+			managerTesorero.ingresarTipoGasto(tipoGasto);
+			listaTipoGastos = managerTesorero.findAllTipoGastos();
+			tipoGasto = new TipoGasto();
+			JSFUtil.crearMensajeInfo("Tipo Gasto creado correctamente");
+		} catch (Exception e) {
+			e.printStackTrace();
+			listaTipoGastos = managerTesorero.findAllTipoGastos();
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+	
+	public void actionListenerEditarTipoGasto() {
+		try {
+			managerTesorero.editarTipoGasto(editarTipoGasto);
+			listaTipoGastos = managerTesorero.findAllTipoGastos();
+			JSFUtil.crearMensajeInfo("Tipo Gasto editado correctamente.!");
+		} catch (Exception e) { 
+			e.printStackTrace();
+			listaTipoGastos = managerTesorero.findAllTipoGastos();
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public void actionListenerEliminarTipoGasto(long idTipoGasto) {
+		try {
+			managerTesorero.eliminarTipoGasto(idTipoGasto);
+			listaTipoGastos = managerTesorero.findAllTipoGastos();
+			JSFUtil.crearMensajeInfo("Tipo Gasto eliminado correctamente");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public List<TipoGasto> getListaTipoGastos() {
+		return listaTipoGastos;
+	}
+
+	public void setListaTipoGastos(List<TipoGasto> listaTipoGastos) {
+		this.listaTipoGastos = listaTipoGastos;
+	}
+
+	public TipoGasto getTipoGasto() {
+		return tipoGasto;
+	}
+
+	public void setTipoGasto(TipoGasto tipoGasto) {
+		this.tipoGasto = tipoGasto;
+	}
+
+	public TipoGasto getEditarTipoGasto() {
+		return editarTipoGasto;
+	}
+
+	public void setEditarTipoGasto(TipoGasto editarTipoGasto) {
+		this.editarTipoGasto = editarTipoGasto;
+	}
+
+}
--- condominioWeb/src/condominio/modulos/usuario/view/controller/BeanUsuario.java
+++ condominioWeb/src/condominio/modulos/usuario/view/controller/BeanUsuario.java
@@ -18,177 +18,194 @@ import java.util.List;
 @ViewScoped
 public class BeanUsuario implements Serializable {
 	private static final long serialVersionUID = 1L;
-private Usuario usuario=new Usuario();
-private Usuario editarUsuario=new Usuario();
-private long idrolfk;
-private long idrolfkE;
-private Rol r=new Rol();
-private Rol editarRol=new Rol();
-private  List<Rol> listaRoles;
-private  List<Usuario> listaUsuarios;
+	private Usuario usuario = new Usuario();
+	private Usuario editarUsuario = new Usuario();
+	private long idrolfk;
+	private long idrolfkE;
+	private Rol r = new Rol();
+	private Rol editarRol = new Rol();
+	private List<Rol> listaRoles;
+	private List<Usuario> listaUsuarios;
 
-@EJB ManagerUsuario managerUsuario;
+	@EJB
+	ManagerUsuario managerUsuario;
 
-@PostConstruct
-public void init() {
-	try {
-	listaRoles=managerUsuario.findAllRoles();
-	listaUsuarios=managerUsuario.findAllUsuarios();
+	@PostConstruct
+	public void init() {
+		try {
+			listaRoles = managerUsuario.findAllRoles();
+			listaUsuarios = managerUsuario.findAllUsuarios();
+
+		} catch (Exception e) {
+			e.getStackTrace();
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+
+	}
+
+	public void actionListenerIngresarRol() {
+		try {
+			managerUsuario.ingresarRol(r);
+			listaRoles = managerUsuario.findAllRoles();
+			r = new Rol();
+			JSFUtil.crearMensajeInfo("Rol creado correctamente");
+		} catch (Exception e) {
+			e.printStackTrace();
+			listaRoles = managerUsuario.findAllRoles();
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public BeanUsuario() {
+
+	}
+
+	public void actionListenerCargarRol(Rol r) {
+		editarRol = r;
+
+	}
+
+	public void actionListenerIngresarUsuario() {
+		try {
+			usuario.setContrasenia(Seguridad.encriptar(usuario.getCedula()));
+			managerUsuario.ingresarUsuario(usuario, idrolfk);
+			listaUsuarios = managerUsuario.findAllUsuarios();
+			usuario = new Usuario();
+			JSFUtil.crearMensajeInfo("Usuario creado correctamente");
+		} catch (Exception e) {
+			e.printStackTrace();
+			listaUsuarios = managerUsuario.findAllUsuarios();
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public void actionListenerCargarUsuario(Usuario u) {
+		editarUsuario = u;
+		idrolfkE = u.getRol().getIdrol();
+	}
+
+	public void actionListenerEditarRol() {
+		try {
+			managerUsuario.editarRol(editarRol);
+			listaRoles = managerUsuario.findAllRoles();
+			JSFUtil.crearMensajeInfo("Rol editado correctamente.!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			listaRoles = managerUsuario.findAllRoles();
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public void actionListenerEditarUsuario() {
+		try {
+
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public void actionListenerEliminarUsuario(long idUsuario) {
+		try {
+			managerUsuario.EliminarUsuario(idUsuario);
+			listaUsuarios = managerUsuario.findAllUsuarios();
+			JSFUtil.crearMensajeInfo("Usuario eliminado correctamente");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public void actionListenerEliminarRol(long idRol) {
+		try {
+			managerUsuario.EliminarRol(idRol);
+			listaRoles = managerUsuario.findAllRoles();
+			JSFUtil.crearMensajeInfo("Rol eliminado correctamente");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+		}
+	}
+
+	public String actionRedireccionarRoles() {
+		return "roles.xhtml?faces-redirect=true";
+	}
+
+	public String actionRedireccionarUsuarios() {
+		return "usuarios.xhtml?faces-redirect=true";
+	}
+
+	public String actionRedireccionarGastos() {
+		return "gastos.xhtml?faces-redirect=true";
+	}
 	
-	} catch (Exception e) {
-		e.getStackTrace();
-		JSFUtil.crearMensajeError(e.getMessage());
+	public String actionRedireccionarTipoGastos() {
+		return "tipogastos.xhtml?faces-redirect=true";
 	}
 
-}
-public void actionListenerIngresarRol() {
-	try {
-		managerUsuario.ingresarRol(r);
-		listaRoles=managerUsuario.findAllRoles();
-		r=new Rol();
-		JSFUtil.crearMensajeInfo("Rol creado correctamente");
-	} catch (Exception e) {
-		e.printStackTrace();
-		listaRoles=managerUsuario.findAllRoles();
-		JSFUtil.crearMensajeError(e.getMessage());
+	public void actionRegistrarUsurio() {
+
 	}
-}
-public BeanUsuario() {
-	 
-}
-public void actionListenerCargarRol(Rol r) {
-	editarRol=r;
-	
-	
-}
-public void actionListenerIngresarUsuario() {
-	try {
-		usuario.setContrasenia(Seguridad.encriptar(usuario.getCedula()));
-		managerUsuario.ingresarUsuario(usuario, idrolfk);
-		listaUsuarios=managerUsuario.findAllUsuarios();
-		usuario=new Usuario();
-		JSFUtil.crearMensajeInfo("Usuario creado correctamente");
-	} catch (Exception e) {
-		e.printStackTrace();
-		listaUsuarios=managerUsuario.findAllUsuarios();
-		JSFUtil.crearMensajeError(e.getMessage());
+
+	public Usuario getUsuario() {
+		return usuario;
 	}
-}
-public void actionListenerCargarUsuario(Usuario u) {
-	editarUsuario=u;
-	idrolfkE=u.getRol().getIdrol();
-}
-public void actionListenerEditarRol() {
-	try {
-		managerUsuario.editarRol(editarRol);
-		listaRoles=managerUsuario.findAllRoles();
-		JSFUtil.crearMensajeInfo("Rol editado correctamente.!");
-	} catch (Exception e) { 
-		e.printStackTrace();
-		listaRoles=managerUsuario.findAllRoles();
-		JSFUtil.crearMensajeError(e.getMessage());
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
-}
-public void actionListenerEditarUsuario() {
-	try {
-		
-	} catch (Exception e) {
-		JSFUtil.crearMensajeError(e.getMessage());
+
+	public long getIdrolfk() {
+		return idrolfk;
 	}
-}
-public void actionListenerEliminarUsuario(long idUsuario) {
-	try {
-		managerUsuario.EliminarUsuario(idUsuario);
-		listaUsuarios=managerUsuario.findAllUsuarios();
-		JSFUtil.crearMensajeInfo("Usuario eliminado correctamente");
-	} catch (Exception e) {
-		JSFUtil.crearMensajeError(e.getMessage());
+
+	public void setIdrolfk(long idrolfk) {
+		this.idrolfk = idrolfk;
 	}
-}
-public void actionListenerEliminarRol(long idRol) {
-	try {
-		managerUsuario.EliminarRol(idRol);
-		listaRoles=managerUsuario.findAllRoles();
-		JSFUtil.crearMensajeInfo("Rol eliminado correctamente");
-	} catch (Exception e) {
-		JSFUtil.crearMensajeError(e.getMessage());
+
+	public List<Rol> getListaRoles() {
+		return listaRoles;
 	}
-}
 
+	public void setListaRoles(List<Rol> listaRoles) {
+		this.listaRoles = listaRoles;
+	}
 
+	public List<Usuario> getListaUsuarios() {
+		return listaUsuarios;
+	}
 
-public String actionRedireccionarRoles() {
-	return"roles.xhtml?faces-redirect=true";
-}
-public String actionRedireccionarUsuarios() {
-	return"usuarios.xhtml?faces-redirect=true";
-}
-public String actionRedireccionarGastos() {
-	return"gastos.xhtml?faces-redirect=true";
-}
- public void actionRegistrarUsurio() {
-	 
- }
- 
-public Usuario getUsuario() {
-	return usuario;
-}
-public void setUsuario(Usuario usuario) {
-	this.usuario = usuario;
-}
-public long getIdrolfk() {
-	return idrolfk;
-}
-public void setIdrolfk(long idrolfk) {
-	this.idrolfk = idrolfk;
-}
+	public void setListaUsuarios(List<Usuario> listaUsuarios) {
+		this.listaUsuarios = listaUsuarios;
+	}
 
+	public Usuario getEditarUsuario() {
+		return editarUsuario;
+	}
 
- 
+	public void setEditarUsuario(Usuario editarUsuario) {
+		this.editarUsuario = editarUsuario;
+	}
 
+	public Rol getEditarRol() {
+		return editarRol;
+	}
 
-public List<Rol> getListaRoles() {
-	return listaRoles;
-}
+	public void setEditarRol(Rol editarRol) {
+		this.editarRol = editarRol;
+	}
 
+	public Rol getR() {
+		return r;
+	}
 
-public void setListaRoles(List<Rol> listaRoles) {
-	this.listaRoles = listaRoles;
-}
+	public void setR(Rol r) {
+		this.r = r;
+	}
 
+	public long getIdrolfkE() {
+		return idrolfkE;
+	}
 
-public List<Usuario> getListaUsuarios() {
-	return listaUsuarios;
-}
+	public void setIdrolfkE(long idrolfkE) {
+		this.idrolfkE = idrolfkE;
+	}
 
-
-public void setListaUsuarios(List<Usuario> listaUsuarios) {
-	this.listaUsuarios = listaUsuarios;
-}
-public Usuario getEditarUsuario() {
-	return editarUsuario;
-}
-public void setEditarUsuario(Usuario editarUsuario) {
-	this.editarUsuario = editarUsuario;
-}
-public Rol getEditarRol() {
-	return editarRol;
-}
-public void setEditarRol(Rol editarRol) {
-	this.editarRol = editarRol;
-}
-public Rol getR() {
-	return r;
-}
-public void setR(Rol r) {
-	this.r = r;
-}
-public long getIdrolfkE() {
-	return idrolfkE;
-}
-public void setIdrolfkE(long idrolfkE) {
-	this.idrolfkE = idrolfkE;
-}
-	
- 
 }
