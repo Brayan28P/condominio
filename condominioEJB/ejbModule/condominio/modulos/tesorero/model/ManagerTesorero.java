521f8d6b77bda75ae1099f783c0ecc2beb3f743a
Guido Calderón <guical_96@hotmail.com>
2020-09-13 17:32:59 -0500

    CRUD tipo gasto

--- condominioEJB/ejbModule/condominio/modulos/tesorero/model/ManagerTesorero.java
+++ condominioEJB/ejbModule/condominio/modulos/tesorero/model/ManagerTesorero.java
@@ -9,6 +9,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import condominio.core.model.entities.Gasto;
+import condominio.core.model.entities.TipoGasto;
 
 /**
  * Session Bean implementation class ManagerTesorero
@@ -30,7 +31,7 @@ public class ManagerTesorero {
 	 */
 	@SuppressWarnings("unchecked")
 	public List<Gasto> findAllGastos() {
-		Query q = em.createQuery("SELECT r FROM Gasto r order by r.idgasto asc", Gasto.class);
+		Query q = em.createQuery("SELECT r FROM Gasto r order by r.idgastoc asc", Gasto.class);
 		List<Gasto> lista = q.getResultList();
 		return lista;
 	}
@@ -76,6 +77,7 @@ public class ManagerTesorero {
 
 	/**
 	 * Eliminar Gasto
+	 * 
 	 * @param idGasto
 	 * @throws Exception
 	 */
@@ -85,6 +87,90 @@ public class ManagerTesorero {
 		} else {
 			Gasto g = new Gasto();
 			g = findGastoById(idGasto);
+			if (g == null) {
+				throw new Exception("No se ha encontrado gasto");
+			} else {
+				em.remove(g);
+			}
+		}
+	}
+
+	/**
+	 * Lista de tipo de gastos
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Gasto> findAllTipoGastos() {
+		Query q = em.createQuery("SELECT r FROM tipo_gasto r order by r.idgasto asc", Gasto.class);
+		List<Gasto> lista = q.getResultList();
+		return lista;
+	}
+
+	/**
+	 * Crear nuevo tipo gasto
+	 * 
+	 * @param gasto
+	 * @throws Exception
+	 */
+	public void ingresarTipoGasto(TipoGasto gasto) throws Exception {
+		if (gasto != null) {
+			TipoGasto g = new TipoGasto();
+			g = findTipoGastoById(gasto.getIdgasto());
+			if (g == null) {
+				em.persist(gasto);
+			} else if (g.getIdgasto() != gasto.getIdgasto()) {
+				throw new Exception("Ya existe un tipo gasto con ese nombre!");
+			}
+			em.persist(gasto);
+		} else {
+			throw new Exception("El objeto Tipo Gasto no ha sido cargado correctamente.!");
+		}
+	}
+
+	/**
+	 * Encontrar Tipo Gasto por ID
+	 * 
+	 * @param idGasto
+	 * @return
+	 */
+	public TipoGasto findTipoGastoById(long idGasto) {
+		TipoGasto gasto = em.find(TipoGasto.class, idGasto);
+		return gasto;
+	}
+
+	/**
+	 * Editar Tipo Gasto
+	 * 
+	 * @param g
+	 * @throws Exception
+	 */
+	public void editarTipoGasto(TipoGasto g) throws Exception {
+		if (g == null) {
+			throw new Exception("El objeto Tipo Gasto no ha sido cargado correctamente.!");
+		} else {
+			TipoGasto gasto = new TipoGasto();
+			gasto = findTipoGastoById(g.getIdgasto());
+			if (gasto == null) {
+				em.merge(g);
+			} else if (g.getIdgasto() != gasto.getIdgasto()) {
+				throw new Exception("Ya existe un tipo gasto con ese nombre!");
+			}
+		}
+	}
+
+	/**
+	 * Eliminar Tipo Gasto
+	 * 
+	 * @param idGasto
+	 * @throws Exception
+	 */
+	public void eliminarTipoGasto(long idGasto) throws Exception {
+		if (idGasto == 0) {
+			throw new Exception("El Tipo Gasto no existe vuelva a intentarlo");
+		} else {
+			TipoGasto g = new TipoGasto();
+			g = findTipoGastoById(idGasto);
 			if (g == null) {
 				throw new Exception("No se ha encontrado gasto");
 			} else {
