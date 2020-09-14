c8b3feaa9e42e19046d36fbba2b36543d0123a66
Guido Calderón <guical_96@hotmail.com>
2020-09-13 21:12:22 -0500

    Tipo pago

--- condominioEJB/ejbModule/condominio/modulos/tesorero/model/ManagerTesorero.java
+++ condominioEJB/ejbModule/condominio/modulos/tesorero/model/ManagerTesorero.java
@@ -11,6 +11,7 @@ import javax.persistence.Query;
 import condominio.core.model.entities.Gasto;
 import condominio.core.model.entities.Rol;
 import condominio.core.model.entities.TipoGasto;
+import condominio.core.model.entities.TipoPago;
 
 /**
  * Session Bean implementation class ManagerTesorero
@@ -47,7 +48,7 @@ public class ManagerTesorero {
 		if (gasto != null) {
 			em.persist(gasto);
 		} else {
-			throw new Exception("El objeto Gasto no ha sido cargado correctamente.!");
+			throw new Exception("El objeto gasto no ha sido cargado correctamente.!");
 		}
 	}
 
@@ -70,7 +71,7 @@ public class ManagerTesorero {
 	 */
 	public void editarGasto(Gasto g) throws Exception {
 		if (g == null) {
-			throw new Exception("El objeto Gasto no ha sido cargado correctamente.!");
+			throw new Exception("El objeto gasto no ha sido cargado correctamente.!");
 		} else {
 			em.merge(g);
 		}
@@ -84,7 +85,7 @@ public class ManagerTesorero {
 	 */
 	public void eliminarGasto(long idGasto) throws Exception {
 		if (idGasto == 0) {
-			throw new Exception("El Gasto no existe vuelva a intentarlo");
+			throw new Exception("El tipo gasto no existe vuelva a intentarlo");
 		} else {
 			Gasto g = new Gasto();
 			g = findGastoById(idGasto);
@@ -126,7 +127,7 @@ public class ManagerTesorero {
 				}
 			}
 		} else {
-			throw new Exception("El objeto Tipo Gasto no ha sido cargado correctamente.!");
+			throw new Exception("El objeto tipo gasto no ha sido cargado correctamente.!");
 		}
 	}
 
@@ -170,7 +171,7 @@ public class ManagerTesorero {
 	 */
 	public void editarTipoGasto(TipoGasto g) throws Exception {
 		if (g == null) {
-			throw new Exception("El objeto Tipo Gasto no ha sido cargado correctamente.!");
+			throw new Exception("El objeto tipo gasto no ha sido cargado correctamente.!");
 		} else {
 			TipoGasto gasto = new TipoGasto();
 			gasto = findTipoGastoByNombre(g.getNombre());
@@ -190,12 +191,118 @@ public class ManagerTesorero {
 	 */
 	public void eliminarTipoGasto(long idGasto) throws Exception {
 		if (idGasto == 0) {
-			throw new Exception("El Tipo Gasto no existe vuelva a intentarlo");
+			throw new Exception("El tipo gasto no existe vuelva a intentarlo");
 		} else {
 			TipoGasto g = new TipoGasto();
 			g = findTipoGastoById(idGasto);
 			if (g == null) {
-				throw new Exception("No se ha encontrado gasto");
+				throw new Exception("No se ha encontrado tipo gasto");
+			} else {
+				em.remove(g);
+			}
+		}
+	}
+	
+	/**
+	 * Lista de tipo de pago
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<TipoPago> findAllTipoPagos() {
+		Query q = em.createQuery("SELECT r FROM TipoPago r order by r.idPago asc", TipoPago.class);
+		List<TipoPago> lista = q.getResultList();
+		return lista;
+	}
+
+	/**
+	 * Crear nuevo tipo Pago
+	 * 
+	 * @param Pago
+	 * @throws Exception
+	 */
+	public void ingresarTipoPago(TipoPago pago) throws Exception {
+		if (pago != null) {
+			{
+				TipoPago g = new TipoPago();
+				g = findTipoPagoByNombre(pago.getNombre());
+				if (g == null) {
+					em.persist(pago);
+				} else {
+					throw new Exception("Ya existe un tipo pago con ese nombre!");
+				}
+			}
+		} else {
+			throw new Exception("El objeto tipo pago no ha sido cargado correctamente.!");
+		}
+	}
+
+	/**
+	 * Encontrar Tipo Pago por ID
+	 * 
+	 * @param idPago
+	 * @return
+	 */
+	public TipoPago findTipoPagoById(long idPago) {
+		TipoPago Pago = em.find(TipoPago.class, idPago);
+		return Pago;
+	}
+
+	/**
+	 * Encontrar Tipo Pago por Nombre
+	 * 
+	 * @param nombre
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public TipoPago findTipoPagoByNombre(String nombre) {
+		Query q = em.createQuery("SELECT r FROM TipoPago r " + " where r.nombre=?1", TipoPago.class);
+		q.setParameter(1, nombre);
+		TipoPago r = new TipoPago();
+		List<TipoPago> lista = q.getResultList();
+		if (lista.isEmpty()) {
+			return null;
+		} else {
+			r = lista.get(0);
+			return r;
+		}
+
+	}
+
+	/**
+	 * Editar Tipo Pago
+	 * 
+	 * @param g
+	 * @throws Exception
+	 */
+	public void editarTipoPago(TipoPago g) throws Exception {
+		if (g == null) {
+			throw new Exception("El objeto tipo pago no ha sido cargado correctamente.!");
+		} else {
+			TipoPago Pago = new TipoPago();
+			Pago = findTipoPagoByNombre(g.getNombre());
+			if (Pago == null) {
+				em.merge(g);
+			} else if (g.getIdpago() != Pago.getIdpago()) {
+				throw new Exception("Ya existe un tipo pago con ese nombre!");
+			}
+		}
+	}
+
+	/**
+	 * Eliminar Tipo Pago
+	 * 
+	 * @param idPago
+	 * @throws Exception
+	 */
+	public void eliminarTipoPago(long idPago) throws Exception {
+		if (idPago == 0) {
+			throw new Exception("El tipo pago no existe vuelva a intentarlo");
+		} else {
+			TipoPago g = new TipoPago();
+			g = findTipoPagoById(idPago);
+			if (g == null) {
+				throw new Exception("No se ha encontrado tipo pago");
 			} else {
 				em.remove(g);
 			}
