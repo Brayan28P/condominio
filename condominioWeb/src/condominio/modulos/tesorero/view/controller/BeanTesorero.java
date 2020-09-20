8365edd2398e88d99f7033f1ea83ea6c84ed0c0e
Guytho1996 <guical_96@hotmail.com>
2020-09-20 17:17:57 -0500

    Pagos condominio

--- condominioWeb/src/condominio/modulos/tesorero/view/controller/BeanTesorero.java
+++ condominioWeb/src/condominio/modulos/tesorero/view/controller/BeanTesorero.java
@@ -6,10 +6,14 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.model.chart.PieChartModel;
+
 import condominio.core.model.entities.Gasto;
+import condominio.core.model.entities.PagoCondominio;
 import condominio.core.model.entities.TipoGasto;
 import condominio.core.model.entities.Usuario;
 import condominio.modulos.login.view.controller.BeanLogin;
+import condominio.modulos.tesorero.model.ManagerPagoTesorero;
 import condominio.modulos.tesorero.model.ManagerTesorero;
 import condominio.modulos.usuario.model.ManagerUsuario;
 import condominio.modulos.util.view.controller.JSFUtil;
@@ -30,8 +34,14 @@ public class BeanTesorero implements Serializable {
 	private Gasto editarGasto = new Gasto();
 	private long idtgastofk;
 	private long idtgastofkE;
+	private PieChartModel pieModel;
+	private double ingresos=0;
+	private double gastos=0;
+	private double total=0;
 	@EJB
 	ManagerTesorero managerTesorero;
+	@EJB
+	ManagerPagoTesorero managerPagos;
 	@EJB
 	ManagerUsuario managerUsuario;
 	@Inject
@@ -45,6 +55,7 @@ public class BeanTesorero implements Serializable {
 			e.getStackTrace();
 			JSFUtil.crearMensajeError(e.getMessage());
 		}
+		createPieModel();
 
 	}
 
@@ -99,6 +110,30 @@ public class BeanTesorero implements Serializable {
 			JSFUtil.crearMensajeError(e.getMessage());
 		}
 	}
+	
+	private void createPieModel() {
+        pieModel= new PieChartModel();
+        List<PagoCondominio> listaPagos=managerPagos.findAllPagoCondominos();
+        for(int i=0; i<listaPagos.size(); i++) {
+        	if(listaPagos.get(i).getValido()) {
+        	ingresos+=listaPagos.get(i).getValor().doubleValue();
+        	}
+        }
+        for(int i=0; i<listaGastos.size(); i++) {
+        	gastos+=listaGastos.get(i).getValor().doubleValue();
+        }
+        
+        total=ingresos-gastos;
+        pieModel.set("Ingresos", ingresos);
+        pieModel.set("Gastos", gastos);
+        pieModel.setShowDataLabels(true);
+        pieModel.setSeriesColors("75DBFF ,FF757C, 0A0000");
+        pieModel.setNegativeSeriesColors("000000");
+        pieModel.setTitle("Saldo");
+        pieModel.setLegendPosition("w");
+        pieModel.setShadow(false);
+        pieModel.setSliceMargin(5);
+    }
 
 	public List<Gasto> getListaGastos() {
 		return listaGastos;
@@ -138,6 +173,38 @@ public class BeanTesorero implements Serializable {
 
 	public void setIdtgastofkE(long idtgastofkE) {
 		this.idtgastofkE = idtgastofkE;
+	}
+
+	public PieChartModel getPieModel() {
+		return pieModel;
+	}
+
+	public void setPieModel(PieChartModel pieModel) {
+		this.pieModel = pieModel;
+	}
+
+	public double getIngresos() {
+		return ingresos;
+	}
+
+	public void setIngresos(double ingresos) {
+		this.ingresos = ingresos;
+	}
+
+	public double getGastos() {
+		return gastos;
+	}
+
+	public void setGastos(double gastos) {
+		this.gastos = gastos;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
 	}
 
 	
