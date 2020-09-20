f8d963177d1288c4846c523bc22e3ce02baf962f
Guytho1996 <guical_96@hotmail.com>
2020-09-20 17:59:27 -0500

    Mensaje eliminar

--- condominioEJB/ejbModule/condominio/modulos/condominio/model/ManagerCondominio.java
+++ condominioEJB/ejbModule/condominio/modulos/condominio/model/ManagerCondominio.java
@@ -8,7 +8,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import condominio.core.model.entities.PagoCondominio;
 import condominio.core.model.entities.Usuario;
 
 /**
@@ -36,79 +35,5 @@ public class ManagerCondominio {
 		List<Usuario> lista = q.getResultList();
 		return lista;
 	}
-	
-	/**
-	 * Lista de PagoCondominos
-	 * 
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public List<PagoCondominio> findAllPagoCondominos(long idUsuario) {
-		Query q = em.createQuery("SELECT r FROM PagoCondominio r where r.usuario.idusuario=?1  order by r.valido asc", PagoCondominio.class);
-		q.setParameter(1, idUsuario);
-		List<PagoCondominio> lista = q.getResultList();
-		return lista;
-	}
-
-	/**
-	 * Crear nuevo PagoCondominio
-	 * 
-	 * @param PagoCondominio
-	 * @throws Exception
-	 */
-	public void ingresarPagoCondomino(PagoCondominio PagoCondominio) throws Exception {
-		if (PagoCondominio != null) {
-			em.persist(PagoCondominio);
-		} else {
-			throw new Exception("El objeto pago condominio no ha sido cargado correctamente.!");
-		}
-	}
-
-	/**
-	 * Encontrar PagoCondominio por ID
-	 * 
-	 * @param idPagoCondomino
-	 * @return
-	 */
-	public PagoCondominio findPagoCondominoById(long idPagoCondomino) {
-		PagoCondominio PagoCondominio = em.find(PagoCondominio.class, idPagoCondomino);
-		return PagoCondominio;
-	}
-
-	/**
-	 * Editar PagoCondominio
-	 * 
-	 * @param g
-	 * @throws Exception
-	 */
-	public void editarPagoCondomino(PagoCondominio g) throws Exception {
-		if (g == null) {
-			throw new Exception("El objeto pago condomino no ha sido cargado correctamente.!");
-		} else {
-			em.merge(g);
-		}
-	}
-
-	/**
-	 * Eliminar PagoCondominio
-	 * 
-	 * @param idPagoCondomino
-	 * @throws Exception
-	 */
-	public void eliminarPagoCondomino(long idPagoCondomino) throws Exception {
-		if (idPagoCondomino == 0) {
-			throw new Exception("El tipo pago condomino no existe vuelva a intentarlo");
-		} else {
-			PagoCondominio g = new PagoCondominio();
-			g = findPagoCondominoById(idPagoCondomino);
-			if (g == null) {
-				throw new Exception("No se ha encontrado pago condominio");
-			} else {
-				em.remove(g);
-			}
-		}
-	}
-	
-	
 
 }
